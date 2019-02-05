8da24e690492b3faff4ee3fd85ff1ae23e5a5652
Maged Makramalla <m.makramalla@gmail.com>
2019-02-05 10:44:08 -0500

    improved only_games script

--- databse/only_barcelona_games.py
+++ databse/only_barcelona_games.py
@@ -3,25 +3,42 @@ import pandas as pd
 df = pd.read_csv('Match.csv')
 #print(df.head())
 
+team_id = 8634
+
 #df_BAR = df[df['home_team_api_id'] != 8634]
 #df_BAR = df[df['country_id'] != 1]
 
-df_BAR = df.loc[(df['home_team_api_id'] == 8634) | (df['away_team_api_id'] == 8634) ]
-#df_BAR = df.loc[(df['home_team_api_id'] == 8634)
-df_BAR ["home_win"] =0
-df_BAR ["away_win"] =0
+df_BAR = df.loc[(df['home_team_api_id'] == team_id) | (df['away_team_api_id'] == team_id) ]
+
+df_BAR ["team_goal_scored"] =0
+df_BAR ["team_goal_conceded"] =0
+df_BAR ["team_win"] =0
+df_BAR ["team_loss"] =0
 df_BAR ["draw"] =0
 
 
 for index, row in df_BAR.iterrows():
-  goal_diff = df_BAR.at[index, "home_team_goal"] - df_BAR.at[index, "away_team_goal"]
   
-  if goal_diff > 0:
-    df_BAR.at[index, "home_win"] = 1
-  elif goal_diff ==0:
-    df_BAR.at[index, "draw"] = 1
+
+  if df_BAR.at[index, 'home_team_api_id'] == team_id:
+    team_goal_scored = df_BAR.at[index, "home_team_goal"]
+    team_goal_conceded = df_BAR.at[index, "away_team_goal"]
   else:
-    df_BAR.at[index, "away_win"] = 1
+    team_goal_scored = df_BAR.at[index, "away_team_goal"]
+    team_goal_conceded = df_BAR.at[index, "home_team_goal"]
+
+  df_BAR.at[index, 'team_goal_scored'] = team_goal_scored
+  df_BAR.at[index, 'team_goal_conceded'] = team_goal_conceded
+
+  goal_diff = team_goal_scored - team_goal_conceded
+
+  if goal_diff > 0:
+    df_BAR.at[index, "team_win"] = 1
+  elif goal_diff < 0:
+    df_BAR.at[index, "team_loss"] = 1
+  else:
+    df_BAR.at[index, "draw"] = 1
+
 
 print (df_BAR)
 
